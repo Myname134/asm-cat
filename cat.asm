7f2521e48fc787f64f4fae81ec6a2d651c63cba9
Bhuvan Sreedharam <200482627+Myname134@users.noreply.github.com>
2026-01-29 14:16:25 +0530

    Added file input support to cat implementation.

--- cat.asm
+++ cat.asm
@@ -1,30 +1,49 @@
 global _start
-section .bss 
-  buffer resb 4096 
+section .bss
+  buffer resb 4096
+  fd resq 1 
 
 section .text 
 _start:
-.read_loop:
+  mov rax, [rsp]
+  cmp rax, 2
+  jl use_stdin 
+
+  mov rdi, [rsp+16]
+  mov rax, 2
+  xor rsi, rsi 
+  syscall 
+
+  cmp rax, 0
+  jl exit 
+
+  mov [rel fd], rax 
+  jmp read_loop 
+
+use_stdin: 
+  mov qword [rel fd], 0
+
+read_loop:
   mov rax, 0
-  mov rdi, 0
+  mov rdi, [rel fd]
   mov rsi, buffer 
   mov rdx, 4096 
   syscall 
 
   cmp rax, 0
-  je .exit 
-  jl .exit 
+  jle exit 
 
   mov rdi, 1
   mov rdx, rax 
   mov rax, 1
   syscall 
 
-  jmp .read_loop 
+  jmp read_loop 
 
-.exit: 
+exit:
   mov rax, 60
   xor rdi, rdi 
   syscall 
+
 
 
