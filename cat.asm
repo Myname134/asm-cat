138204199a7e3b31f0f1bba631e972a34c39d6ce
Bhuvan Sreedharam <200482627+Myname134@users.noreply.github.com>
2026-02-19 21:28:53 +0530

    cat: fix multi-file bug caused by ABI register corruption
    Remove call-based structure and rewrite using jump control
    flow. Iterate argv until NULL and use openat for correctness
    on modern Linux.

--- cat.asm
+++ cat.asm
@@ -1,103 +1,74 @@
-%define STDIN  0
-%define STDOUT 1
-%define STDERR 2
-
-%define SYS_READ  0
-%define SYS_WRITE 1
-%define SYS_OPEN  2
-%define SYS_CLOSE 3
-%define SYS_EXIT  60
-
-%define BUF_SIZE 4096
-
-global _start
+section .data
+    err_open db "Error opening file", 10
+    err_len  equ $ - err_open
 
 section .bss
-buffer resb BUF_SIZE
-fd     resq 1
-
-section .rodata
-open_err db "cat: Cannot open file", 10
-open_err_len equ $ - open_err
+    buffer   resb 4096
 
 section .text
+    global _start
+
 _start:
-    mov rax, [rsp]        
-    cmp rax, 2
-    jl use_stdin
+    mov rbx, [rsp]        ; argc
+    cmp rbx, 2
+    jl exit
 
-    mov rdi, [rsp+16]     
-    mov rax, SYS_OPEN
-    xor rsi, rsi          
+    lea r12, [rsp+16]     ; &argv[1]
+
+next_file:
+    mov rsi, [r12]        ; current argv pointer
+    test rsi, rsi
+    jz exit               ; stop at NULL
+
+    ; openat(AT_FDCWD, filename, O_RDONLY, 0)
+    mov rax, 257
+    mov rdi, -100
+    mov rdx, 0
+    mov r10, 0
     syscall
 
-    cmp rax, 0
-    jl open_failed
+    test rax, rax
+    js open_error
 
-    mov [rel fd], rax
-    jmp read_loop
-
-open_failed:
-    mov rdi, STDERR
-    mov rsi, open_err
-    mov rdx, open_err_len
-    mov rax, SYS_WRITE
-    syscall
-
-   
-    mov rax, [rsp+16]     
-    mov rdi, 1
-    mov rax, SYS_EXIT
-    syscall
-
-use_stdin:
-    mov qword [rel fd], STDIN
+    mov r13, rax          ; fd
 
 read_loop:
-    mov rax, SYS_READ
-    mov rdi, [rel fd]
+    mov rax, 0            ; read
+    mov rdi, r13
     mov rsi, buffer
-    mov rdx, BUF_SIZE
+    mov rdx, 4096
     syscall
 
-    cmp rax, 0
-    jle exit_file
+    test rax, rax
+    jle close_file
 
-    
-    mov rcx, rax
-    mov rbx, buffer
-
-write_loop:
-    mov rax, SYS_WRITE
-    mov rdi, STDOUT
-    mov rsi, rbx
-    mov rdx, rcx
+    mov rdx, rax
+    mov rax, 1            ; write
+    mov rdi, 1
+    mov rsi, buffer
     syscall
-    cmp rax, 0
-    jl write_failed
-
-    sub rcx, rax
-    add rbx, rax
-    jnz write_loop
-
     jmp read_loop
 
-write_failed:
-    neg rax
-    mov rdi, rax
-    mov rax, SYS_EXIT
+close_file:
+    mov rax, 3
+    mov rdi, r13
     syscall
 
-exit_file:
-    mov rax, [rel fd]
-    cmp rax, STDIN
-    je exit_program
-    mov rdi, rax
-    mov rax, SYS_CLOSE
+    add r12, 8
+    jmp next_file
+
+open_error:
+    mov rax, 1
+    mov rdi, 2
+    mov rsi, err_open
+    mov rdx, err_len
     syscall
 
-exit_program:
-    mov rax, SYS_EXIT
+    add r12, 8
+    jmp next_file
+
+exit:
+    mov rax, 60
     xor rdi, rdi
     syscall
 
